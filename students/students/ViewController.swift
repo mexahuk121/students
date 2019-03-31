f681c22dee6db3cf9c5c2a08e798efe9aab2714f
mexahuk121 <48319692+mexahuk121@users.noreply.github.com>
2019-03-31 18:29:19 +0300

    Add files via upload

---
+++ students/students/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  students
+//
+//  Created by Admin on 31/03/2019.
+//  Copyright © 2019 Admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
